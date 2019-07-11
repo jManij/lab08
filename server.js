4e57cc01446bc243fa9ed06bfd2a624f284e2284
Manish KC <kcmanish89@gmail.com>
2019-07-11 14:34:15 -0700

    Functional except eventbrite doesnot persist yet

--- server.js
+++ server.js
@@ -24,17 +24,14 @@ app.use(cors());
 
 //Routes
 app.get('/location', searchToLatLng);
-
 app.get('/weather', searchWeather);
-
 app.get('/events', searchEvents);
-
 app.use('*', (req, res) => {
   res.send('You got in the wrong place')
 })
 
 
-/*--Functions--*/
+/******************-----CONSTRUCTOR--------**************************/
 
 function FormattedLocation(query, data) {
   this.search_query = query;
@@ -54,54 +51,18 @@ function FormattedEvent(data) {
   this.event_date = new Date(data.start.local).toDateString();
   this.summary = data.description.text;
 }
+/******************-----CONSTRUCTOR--------**************************/
 
+/******************---ROUTES--------**************************/
 function searchToLatLng(request, response) {
   let locationName = request.query.data || 'seattle';
   const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${locationName}&key=${process.env.GEOCODE_API_KEY}`;
 
-  // if is in database
-  // go get it from db
-  client.query(`SELECT * FROM locations WHERE search_query=$1`, [locationName])
-    .then(sqlResult => {
-
-      if (sqlResult.rowCount === 0) {
-        console.log('getting new data from googles');
-        superagent.get(url)
-          .then(result => {
-
-            let location = new FormattedLocation(locationName, result.body)
-
-            // Save the data to postgres
-            //Working
-            // client.query takes two arguments: a sql command, and an array ov values
-            client.query(
-              `INSERT INTO locations (
-          search_query,
-          formatted_query,
-          latitude,
-          longitude
-        ) VALUES ($1, $2, $3, $4)`,
-              [location.search_query, location.formatted_query, location.latitude, location.longitude]
-            )
-            response.send(location);
-
-            console.log(location_id, 'new one')
-
-          }).catch(e => {
-            console.error(e);
-            response.status(500).send('Status 500: So sorry i broke');
-          })
-      } else {
-        console.log('sending from db');
-        // send the frontend what was in the db
-        response.send(sqlResult.rows[0]);
-      }
-    });
-
+  getLocation(locationName, response, url); //Get location
 }
 
+
 function searchWeather(request, response) {
-  // console.log(request.query.data.latitude)
   let locationName = request.query.data.search_query;
   let location_id;
   let lat = request.query.data.latitude;
@@ -113,7 +74,10 @@ function searchWeather(request, response) {
     .then(sqlResult => {
       location_id = sqlResult.rows[0].id; //Gets the recent ID that has been added
       console.log('location ID: ', location_id)
-      addWeatherSendResponse(location_id, response, url);
+      addWeatherSendResponse(location_id, response, url); //Add weather and send response
+    }).catch(e => {
+      console.log(e);
+      response.status(500).send('STATUS 500');
     })
 
 }
@@ -136,7 +100,10 @@ function searchEvents(request, response) {
     })
 }
 
+/******************---ROUTES--------**************************/
 
+
+/******************---HELPER FUNCTIONS--------**************************/
 
 function addWeatherSendResponse(location_id, response, url) {
 
@@ -163,27 +130,56 @@ function addWeatherSendResponse(location_id, response, url) {
           })
 
       } else {
-        console.log('data exists, query here now:');
-        getWeather(location_id, response);
-
+        getWeatherFromDB(location_id, response);
       }
     }).catch(e => {
       console.error(e);
-
     })
 
 }
 
-function getWeather(locationId, response) {
+function getWeatherFromDB(locationId, response) {
   console.log('Sending weather from DB');
   client.query(`SELECT * FROM weather WHERE location_id=$1`, [locationId])
     .then(sqlResult => {
       response.send(sqlResult.rows[0]);
     })
-
-
 }
 
+function getLocation(locationName, response, url) {
+  client.query(`SELECT * FROM locations WHERE search_query=$1`, [locationName])
+    .then(sqlResult => {
+
+      if (sqlResult.rowCount === 0) {
+        console.log('getting new data from googles');
+        superagent.get(url)
+          .then(result => {
+
+            let location = new FormattedLocation(locationName, result.body)
+            client.query(
+              `INSERT INTO locations (
+          search_query,
+          formatted_query,
+          latitude,
+          longitude
+        ) VALUES ($1, $2, $3, $4)`,
+              [location.search_query, location.formatted_query, location.latitude, location.longitude]
+            )
+            response.send(location);
+
+          }).catch(e => {
+            console.error(e);
+            response.status(500).send('Status 500: So sorry i broke');
+          })
+      } else {
+        console.log('sending from db');
+        // send the frontend what was in the db
+        response.send(sqlResult.rows[0]);
+      }
+    });
+}
+
+/******************---HELPER FUNCTIONS--------**************************/
 
 
 //Starting Server
